3c57093000089a6f222b81e112acc1122ceb7c54
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2024-11-27 08:03:26 +0100

    update

--- pages/_1_Comment.py
+++ pages/_1_Comment.py
@@ -53,30 +53,34 @@ client = init_connection()
 db=client['EuthMappers_Forum_241126']
 collection=db['EuthMappers_Forum_241126']
 st.title('What is Sustainable Development ?')
+container1 = st.container()
 def submit_answer():
-    if (comment!="")&(school!=None):
-            st.write(school, comment)
-            post={'school':school,'comment':comment}
-            collection.insert_one(post)
-            st.session_state.answer_submitted=True
-            st.write(st.session_state.answer_submitted)
+        if (comment!="")&(school!=None):
+                st.write(school, comment)
+                post={'school':school,'comment':comment}
+                collection.insert_one(post)
+                st.session_state.answer_submitted=True
+                st.write(st.session_state.answer_submitted)
+        else:
+                st.warning('you have to select your school and write your comment!')
+with container1:
+    st.html("<img src='https://raw.githubusercontent.com/kaheetonaa/streamlit_quizz_template_euth/refs/heads/main/asset/logo.png' class='center'/>")
+    st.markdown(""" ___""")
+
+    if "answer_submitted" not in st.session_state:
+        school = st.selectbox(
+            "Where is your school",
+            ("🇮🇹Italy", "🇵🇹Portugal", "🇷🇴Romania", "🇸🇰Slovakia", "🇪🇸Spain"),index=None
+            )
+
+            #form
+        comment = st.text_input("What is Sustainable Development?", "")
+        st.button('Submit', on_click=submit_answer)
     else:
-            st.warning('you have to select your school and write your comment!')
+        st.balloons()
+        st.write('You have successfully submit your answer!')
 
-if "answer_submitted" not in st.session_state:
-    school = st.selectbox(
-        "Where is your school",
-        ("🇮🇹Italy", "🇵🇹Portugal", "🇷🇴Romania", "🇸🇰Slovakia", "🇪🇸Spain"),index=None
-        )
-
-        #form
-    comment = st.text_input("What is Sustainable Development?", "")
-    st.button('Submit', on_click=submit_answer)
-else:
-    st.balloons()
-    st.write('You have successfully submit your answer!')
-
-    
+        
 
 
 
